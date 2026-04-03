f89ef122f6c3c7c353aed4ca1c4643f3c4d752ca
tiennamz <ghastnam001@gmail.com>
2026-04-03 22:03:41 +0700

    đã xong hết giao diện bắt buộc

--- javaScript/home.js
+++ javaScript/home.js
@@ -6,23 +6,52 @@ let spanHello=document.getElementById("span-hello")
 let spanLogout=document.getElementById("span-logout")
 let spanLogin=document.getElementById("span-login")
 let nameUser=document.getElementById("name")
+let btnAddShedule=document.querySelectorAll(".btn")
 
-if(currentUser==undefined){
-    spanLogin.style.display="inline-block"
-    spanManagerAdmin.style.display="none"
-    spanHello.style.display="none"
-    spanLogout.style.display="none"
-}else{
-    spanLogin.style.display="none"
-    spanHello.style.display="inline-block"
-    spanLogout.style.display="inline-block"
-    nameUser.innerText=`${currentUser.fullname}`
-}
 
-if(currentUser.role=="admin"){
-    spanManagerAdmin.style.display="inline-block"
+// ẩn hiện khi chưa gì đã đăng nhập
+const showLink=()=>{
+    if(currentUser==undefined){
+        spanLogin.style.display="inline-block"
+        spanManagerAdmin.style.display="none"
+        spanHello.style.display="none"
+        spanLogout.style.display="none"
+        return
+    }else{
+        spanLogin.style.display="none"
+        spanHello.style.display="inline-block"
+        spanLogout.style.display="inline-block"
+        nameUser.innerText=`${currentUser.fullname}`
+
+        if(currentUser.role=="admin"){
+            spanManagerAdmin.style.display="inline-block"
+        }
+    }
 }
+showLink()
 
 spanLogout.addEventListener("click",()=>{
     localStorage.removeItem("currentUser")
 })
+
+// sang trang đăng nhập khi chưa đăng nhập
+const navigateLogin=()=>{
+    if(currentUser==undefined){
+        btnAddShedule.forEach((btn)=>{
+            btn.addEventListener("click",()=>{
+                Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Bạn chưa đăng nhập!!! Đang chuyển đến trang đang nhập cho bạn",
+            timer: 5000,
+            timerProgressBar: true,
+                }).then(()=>{
+                    window.location.href="./login.html"
+                })
+            })
+        })
+        return
+    }
+}
+navigateLogin()
+
